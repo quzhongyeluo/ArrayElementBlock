75c3aecbd24a63467b7eefe9aec3e15e5f1122ca
曲终叶落 <quzhongyeluo@quzhongyeluo.local>
2017-08-13 01:35:01 +0800

    创建项目

---
+++ ArrayElementBlock/ArrayElementBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArrayElementBlock
+//
+//  Created by 曲终叶落 on 2017/8/13.
+//  Copyright © 2017年 曲终叶落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ArrayElementBlock/ArrayElementBlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ArrayElementBlock
+//
+//  Created by 曲终叶落 on 2017/8/13.
+//  Copyright © 2017年 曲终叶落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
